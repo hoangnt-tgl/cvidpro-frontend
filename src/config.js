a80416c97bf688101c71f0a0a9bd87846c6f4ae0
Nguyễn Trọng Hoàng <116530644+space-hoang@users.noreply.github.com>
2023-02-26 14:15:47 +0700

    1

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3030/";
-// export const BASE_URL = "https://cvidpro.herokuapp.com/";
+// export const BASE_URL = "http://localhost:3030/";
+export const BASE_URL = "https://cvidpro.herokuapp.com/";
